d0e87373b1ef21503293bd517ca601140ab8fbfc
jngarvit07 <jaingarvit945@gmail.com>
2023-08-09 15:51:49 +0530

    First Commit

---
+++ AJ.java
@@ -0,0 +1,48 @@
+class even_odd
+{
+    public static void main(String args[])
+    {
+        int a=8;
+        if(a%2==0)
+        {
+            System.out.println("Even Number");
+        }
+        else
+        {
+            System.out.println("Odd Number");
+        }
+    }
+}
+
+
+
+
+
+
+
+/*class Main
+{
+	public static void main(String[] args) {
+	//int y=10,z=5,x=20;
+	int i=0,j=9;
+	do{
+	i++;
+	if(j--<i++)
+	{
+	break;
+	}
+	}
+	while(i<5);
+	
+	//for(int k=0;k<20;k=k+2){
+	//if((x>>1==y)&&(y<<1==x))
+	//x++;
+	//}
+	//if(k%3==1)
+	//System.out.println("Possible");
+	//x = y++ + z++;
+	//else
+	System.out.println(i+""+j);
+	}
+}
+*/
